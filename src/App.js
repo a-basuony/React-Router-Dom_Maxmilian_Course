449c3bbb99c5c8812639f9bdc5fd0305eb4b65b5
Ahmed Basuony <99911074+a-basuony@users.noreply.github.com>
2023-07-16 12:58:06 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@
 //    - EventDetailPage
 //    - NewEventPage
 //    - EditEventPage
+//done
 
 // 2. Add routing & route definitions for these five pages
 //    - / => HomePage
@@ -13,10 +14,13 @@
 //    - /events/<some-id> => EventDetailPage
 //    - /events/new => NewEventPage
 //    - /events/<some-id>/edit => EditEventPage
+// done
 
 // 3. Add a root layout that adds the <MainNavigation> component above all page components
 // 4. Add properly working links to the MainNavigation
 // 5. Ensure that the links in MainNavigation receive an "active" class when active
+//done
+
 
 // 6. Output a list of dummy events to the EventsPage
 //    Every list item should include a link to the respective EventDetailPage
